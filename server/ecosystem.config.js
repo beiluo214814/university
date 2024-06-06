1c1e0a888530b7323aee5eb7860ef5ebb9ed5eff
hulijuan <279062301@qq.com>
2024-06-06 15:45:08 +0800

    修改

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'myServer',
     script: 'npm start 118.195.233.117',
-    instances: 4,
+    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
